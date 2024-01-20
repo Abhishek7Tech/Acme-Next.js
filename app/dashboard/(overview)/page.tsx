e2413f185469a9c4ad4e2dd65c9e450e73caeaf5
Abhishek7Tech <abhishektab45@gmail.com>
2024-01-20 14:42:02 +0530

    added metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -10,9 +10,13 @@ import {
   LatestInvoicesSkeleton,
   CardSkeleton,
 } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 
 export default async function Page() {
- 
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -27,8 +31,8 @@ export default async function Page() {
           value={numberOfCustomers}
           type="customers"
         /> */}
-        <Suspense fallback={<CardSkeleton/>}>
-        <CardWrapper/>
+        <Suspense fallback={<CardSkeleton />}>
+          <CardWrapper />
         </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
